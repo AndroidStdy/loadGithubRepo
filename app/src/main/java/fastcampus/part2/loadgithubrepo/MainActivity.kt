81f04cbaa366523cd5f8ed61922113e8217549cd
HunCY5 <90296157+HunCY5@users.noreply.github.com>
2024-07-22 20:49:36 +0900

    retrofit - GitHub API(List Repository) 호출

--- app/src/main/java/fastcampus/part2/loadgithubrepo/MainActivity.kt
+++ app/src/main/java/fastcampus/part2/loadgithubrepo/MainActivity.kt
@@ -1,10 +1,16 @@
 package fastcampus.part2.loadgithubrepo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import fastcampus.part2.loadgithubrepo.model.Repo
+import fastcampus.part2.loadgithubrepo.network.GithubService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -19,5 +25,20 @@ class MainActivity : AppCompatActivity() {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
+        // githubService 구현
+        val githubService = retrofit.create(GithubService::class.java)
+        // api 호출
+        githubService.listRepos("square").enqueue(object: Callback<List<Repo>>{
+            override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
+                //성공할 경우
+                Log.e("MainActivity", response.body().toString())
+            }
+
+            override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
+                //실패할 경우
+
+            }
+
+        })
     }
 }
