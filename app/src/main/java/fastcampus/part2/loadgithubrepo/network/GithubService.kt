85bfc44662612c3fdbed9f6dd91e14ea8a4b9fbb
HunCY5 <90296157+HunCY5@users.noreply.github.com>
2024-07-22 20:57:56 +0900

    retrofit 인터페이스 정의

--- app/src/main/java/fastcampus/part2/loadgithubrepo/network/GithubService.kt
+++ app/src/main/java/fastcampus/part2/loadgithubrepo/network/GithubService.kt
@@ -1,12 +1,20 @@
 package fastcampus.part2.loadgithubrepo.network
 
 import fastcampus.part2.loadgithubrepo.model.Repo
+import fastcampus.part2.loadgithubrepo.model.UserDto
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface GithubService {
 
     @GET("users/{username}/repos")
     fun listRepos(@Path("username") username: String): Call<List<Repo>>
+
+    // search users api
+    @GET("search/users")
+    fun searchUsers(@Query("q") query: String): Call<UserDto>
+
+
 }
