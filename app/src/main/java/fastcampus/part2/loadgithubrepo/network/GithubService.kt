fbcd362fc10441a720d7cad7ea2c13c2e3f0c10f
HunCY5 <90296157+HunCY5@users.noreply.github.com>
2024-07-22 21:04:43 +0900

    GitHub Token 추가

--- app/src/main/java/fastcampus/part2/loadgithubrepo/network/GithubService.kt
+++ app/src/main/java/fastcampus/part2/loadgithubrepo/network/GithubService.kt
@@ -4,10 +4,13 @@ import fastcampus.part2.loadgithubrepo.model.Repo
 import fastcampus.part2.loadgithubrepo.model.UserDto
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface GithubService {
+    //깃허브 토큰 추가
+    @Headers("Authorization: Bearer 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")
 
     @GET("users/{username}/repos")
     fun listRepos(@Path("username") username: String): Call<List<Repo>>
