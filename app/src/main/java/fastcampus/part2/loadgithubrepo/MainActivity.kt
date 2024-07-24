ef45537f811d3d01ee9b938716520c0ccfccab5c
HunCY5 <90296157+HunCY5@users.noreply.github.com>
2024-07-24 17:02:27 +0900

    유저 불러오기 검색 화면 - RecyclerView 적용(adapter 필요)

--- app/src/main/java/fastcampus/part2/loadgithubrepo/MainActivity.kt
+++ app/src/main/java/fastcampus/part2/loadgithubrepo/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import fastcampus.part2.loadgithubrepo.databinding.ActivityMainBinding
 import fastcampus.part2.loadgithubrepo.model.Repo
 import fastcampus.part2.loadgithubrepo.model.UserDto
 import fastcampus.part2.loadgithubrepo.network.GithubService
@@ -16,10 +18,15 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val retrofit = Retrofit.Builder()
             .baseUrl("https://api.github.com/")
@@ -29,7 +36,7 @@ class MainActivity : AppCompatActivity() {
         // githubService 구현
         val githubService = retrofit.create(GithubService::class.java)
         // api 호출
-        githubService.listRepos("square").enqueue(object: Callback<List<Repo>>{
+        githubService.listRepos("square").enqueue(object : Callback<List<Repo>> {
             override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
                 //성공할 경우
                 Log.e("MainActivity", "List Repo : ${response.body().toString()}")
@@ -41,7 +48,7 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
-        githubService.searchUsers("squar").enqueue(object: Callback<UserDto>{
+        githubService.searchUsers("squar").enqueue(object : Callback<UserDto> {
             override fun onResponse(call: Call<UserDto>, response: Response<UserDto>) {
                 //성공할 경우
                 Log.e("MainActivity", "Search User : ${response.body().toString()}")
@@ -52,5 +59,12 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+
+        binding.userRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = 
+        }
+
+
     }
 }
