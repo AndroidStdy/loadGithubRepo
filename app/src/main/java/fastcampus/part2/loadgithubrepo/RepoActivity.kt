970e1ad45180a6d6b3bb2056b4936d60a15ccb12
HunCY5 <90296157+HunCY5@users.noreply.github.com>
2024-07-24 20:07:04 +0900

    RecyclerView - Adapter 생성

--- app/src/main/java/fastcampus/part2/loadgithubrepo/RepoActivity.kt
+++ app/src/main/java/fastcampus/part2/loadgithubrepo/RepoActivity.kt
@@ -3,16 +3,61 @@ package fastcampus.part2.loadgithubrepo
 import android.app.Activity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import fastcampus.part2.loadgithubrepo.adapter.RepoAdapter
 import fastcampus.part2.loadgithubrepo.databinding.ActivityRepoBinding
+import fastcampus.part2.loadgithubrepo.model.Repo
+import fastcampus.part2.loadgithubrepo.network.GithubService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-class RepoActivity: AppCompatActivity() {
+class RepoActivity : AppCompatActivity() {
 
-    private lateinit var  binding: ActivityRepoBinding
+    private lateinit var binding: ActivityRepoBinding
+    private lateinit var repoAdapter: RepoAdapter
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://api.github.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRepoBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        repoAdapter = RepoAdapter()
+
+        binding.repoRecyclerView.apply {
+            layoutManager = LinearLayoutManager(this@RepoActivity)
+            adapter = repoAdapter
+        }
+        listRepo()
     }
+
+    private fun listRepo() {
+        val githubService = retrofit.create(GithubService::class.java)
+        // api 호출
+        githubService.listRepos("square").enqueue(object : Callback<List<Repo>> {
+            override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
+                //성공할 경우
+                Log.e("MainActivity", "List Repo : ${response.body().toString()}")
+
+                repoAdapter.submitList(response.body())
+            }
+
+            override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
+                //실패할 경우
+
+            }
+
+        })
+    }
+
 }
