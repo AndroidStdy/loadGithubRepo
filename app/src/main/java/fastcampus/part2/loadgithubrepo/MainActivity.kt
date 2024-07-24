072383727bdc463c82d4e8edffd220f6837a4825
HunCY5 <90296157+HunCY5@users.noreply.github.com>
2024-07-24 18:36:31 +0900

    유저 불러오기 검색 화면 - 유저 검색 기능 추가 및 XML 오류 수정

--- app/src/main/java/fastcampus/part2/loadgithubrepo/MainActivity.kt
+++ app/src/main/java/fastcampus/part2/loadgithubrepo/MainActivity.kt
@@ -1,11 +1,15 @@
 package fastcampus.part2.loadgithubrepo
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.LinearLayoutManager
 import fastcampus.part2.loadgithubrepo.adapter.UserAdapter
 import fastcampus.part2.loadgithubrepo.databinding.ActivityMainBinding
@@ -21,6 +25,15 @@ import retrofit2.converter.gson.GsonConverterFactory
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var userAdapter: UserAdapter
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://api.github.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val handler = Handler(Looper.getMainLooper())
+    private var searchFor: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,35 +42,48 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://api.github.com/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
 
-        // githubService 구현
-        val githubService = retrofit.create(GithubService::class.java)
-        // api 호출
-        githubService.listRepos("square").enqueue(object : Callback<List<Repo>> {
-            override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
-                //성공할 경우
-                Log.e("MainActivity", "List Repo : ${response.body().toString()}")
-            }
+//        // api 호출
+//        githubService.listRepos("square").enqueue(object : Callback<List<Repo>> {
+//            override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
+//                //성공할 경우
+//                Log.e("MainActivity", "List Repo : ${response.body().toString()}")
+//            }
+//
+//            override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
+//                //실패할 경우
+//
+//            }
+//
+//        })
 
-            override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
-                //실패할 경우
-
-            }
-
-        })
-
-        val userAdapter = UserAdapter()
+        userAdapter = UserAdapter()
 
         binding.userRecyclerView.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = userAdapter
         }
+        val runnable = Runnable {
+            searchUser()
+        }
 
-        githubService.searchUsers("squar").enqueue(object : Callback<UserDto> {
+
+        binding.etSearch.addTextChangedListener {
+
+            searchFor = it.toString() // 검색할 유저 입력
+            handler.removeCallbacks(runnable)
+            handler.postDelayed(
+                runnable,
+                300,
+            )
+        }
+    }
+
+    private fun searchUser() {
+        // githubService 구현
+        val githubService = retrofit.create(GithubService::class.java)
+
+        githubService.searchUsers(searchFor).enqueue(object : Callback<UserDto> {
             override fun onResponse(call: Call<UserDto>, response: Response<UserDto>) {
                 //성공할 경우
                 Log.e("MainActivity", "Search User : ${response.body().toString()}")
@@ -67,10 +93,10 @@ class MainActivity : AppCompatActivity() {
 
             override fun onFailure(call: Call<UserDto>, t: Throwable) {
                 //실패할 경우
+                Toast.makeText(this@MainActivity, "에러가 발생했습니다.", Toast.LENGTH_SHORT).show()
+                t.printStackTrace() //로그로 에러 확인 가능
             }
 
         })
-
-
     }
 }
